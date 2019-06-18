e4f30e4b4f08616d2bc77ba08be227cd05548306
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-06-18 21:12:59 +0300

    Add ComparatorDemo class

---
+++ src/main/java/com/yaroslav/javacore/chapter18/ComparatorDemo.java
@@ -0,0 +1,21 @@
+package com.yaroslav.javacore.chapter18;
+
+import java.util.TreeSet;
+
+public class ComparatorDemo {
+
+    public static void main(String[] args) {
+
+        TreeSet<String> strings = new TreeSet<>(new CustomComparator());
+
+        strings.add("C");
+        strings.add("A");
+        strings.add("E");
+        strings.add("F");
+        strings.add("D");
+        strings.add("G");
+        strings.add("R");
+
+        strings.forEach(System.out::print);
+    }
+}
